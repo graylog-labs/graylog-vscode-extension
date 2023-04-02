b0e5754261c0526a940d3f988d5d719bad6bc2ef
csharpdragon <csdragon0512@yandex.com>
2023-04-02 11:49:51 +0800

    add icon and text color in editor

--- src/extension.ts
+++ src/extension.ts
@@ -6,18 +6,14 @@ import { DepNodeProvider, Item } from './nodeDependencies';
 import { ConnectionPart } from './connectionpart';
 import {GraylogFileSystemProvider} from './fileSystemProvider';
 import { CodelensProvider } from './CodelensProvider';
+
 const colorData = require('../themes/color');
-
-
-
 export function activate(context: vscode.ExtensionContext) {
 
-
+	addColorSettings();
 	const Graylog = new GraylogFileSystemProvider();
 	
 	const connectpart= new ConnectionPart(Graylog,context.secrets);
-	
-	addColorSettings();
 	
 	context.subscriptions.push(vscode.workspace.registerFileSystemProvider('graylog', Graylog, { isCaseSensitive: true }));
 	let initialized = false;
@@ -76,39 +72,17 @@ async function prepareForWork(connectpart:ConnectionPart,secretStorage:vscode.Se
 	
 }
 
+
 function addColorSettings() {
 	(async () => {
-		const config = vscode.workspace.getConfiguration() ;
-		let tokenColorCustomizations = config.inspect('editor.tokenColorCustomizations')?.globalValue;
-
-		const tokenColor = [];
-		const colorDataLength = colorData.length;
-		const tokenColorLength = tokenColor.length;
-
-		for (let i = 0; i < colorDataLength; i++) {
-			const name = colorData[i].name;
-
-			let exist = false;
-			for (let j = 0; j < tokenColorLength; j++) {
-				if (tokenColor[j].name === name) {
-					exist = true;
-					break;
-				}
-			}
-
-			if (!exist) {
-				tokenColor.push(colorData[i]);
-			}
-		}
-
+		const config = vscode.workspace.getConfiguration();
 		await config.update(
 			'editor.tokenColorCustomizations',
-			tokenColorCustomizations,
+			colorData,
 			vscode.ConfigurationTarget.Global,
 		);
 	})();
 }
-
 
 // "views": {
 // 	"package-explorer": [
