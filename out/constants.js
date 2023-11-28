2badb4d7a85551c8539f8639ab3334c8cfb040ed
Ozair <muh.ozair@gmail.com>
2023-11-28 17:24:14 +0500

    create grok deugger webview and settings wizard

--- out/constants.js
+++ out/constants.js
@@ -9,24 +9,19 @@ function newFileSource(title) {
     true
 then
     // Develop the activities to take place within your rule
-    // The Function documentation is here: 
+    // The Function documentation is here:
     // https://go2docs.graylog.org/5-0/making_sense_of_your_log_data/functions_index.html
 
     // The Graylog Information Model (How to name your fields) is here:
     // https://schema.graylog.org
 
     // Thanks for using the Graylog VSCode Editor - Graylog Services Team
-    
+
 end`;
 }
 exports.newFileSource = newFileSource;
 exports.InitGraylogSettingInfo = `{
   "graylogSettings":[
-    {
-      "serverUrl": "",
-      "token": "",
-      "name": ""
-    }
   ]
 }`;
 exports.BASE_PATH = `${vscode?.extensions?.getExtension('pdragon.task-graylog')?.extensionPath}/resources/`;
--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios, { GenericAbortSignal } from 'axios';
 import { newFileSource } from './constants';
 import { sourceError, ServerInfo, Setting } from './interfaces';
 import { MyTreeItem } from './fileSystemProvider';
 import { getFormatedHashValue } from './utils';
 import { crtPath, serverKey } from './constants';
-import * as vscode from 'vscode'
+import * as vscode from 'vscode';
 
 import { promises as fs } from 'fs';
 
@@ -31,7 +31,7 @@ export class API{
                 path = apiPath.substring(0,apiPath.indexOf("/api"));
             }else{
              path = apiPath;}
-            process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+            process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
             const res  = await axios.get(`${path}/api/cluster`, {
                 params: {
                   'pretty': 'true'
@@ -47,7 +47,7 @@ export class API{
               if(Object.keys(res.data).length > 0)
               {
                 return true;
-              }  
+              }
 
               return false;
         }catch(e){
@@ -59,7 +59,7 @@ export class API{
     async testAPI(apiPath:string):Promise<boolean>{
       let res;
         try{
-           process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+           process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
             res  = await axios.get(apiPath);
             if(res.status === 200){    return true; }
             else {return false;}
@@ -67,10 +67,35 @@ export class API{
             return false;
         }
     }
-    
+
+    async testPattern(url: string, token: string, body: Record<string, string | Record<string, string>>, signal: GenericAbortSignal):Promise<Record<string, string>>{
+        try{
+          process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+          const response = await axios.post(`${url}/api/system/grok/test`, body, {
+            headers: {
+              'Content-Type': 'application/json',
+              'x-requested-by': 'XMLHttpRequest'
+            },
+            auth: {
+              username: token,
+              password: this.accountPassword
+            },
+            signal,
+          });
+          return {...response.data, success: true};
+        }catch(e){
+          if(e.response?.data?.message){
+            throw new Error(e.response.data.message);
+          } else {
+            console.log({e});
+          }
+        }
+        return {};
+    }
+
     async getRuleSource(instanceIndex:number,id:string){
         try{
-         process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+         process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
           const response = await axios.get(`${this.apis.serverList[instanceIndex]['serverUrl']}/api/system/pipelines/rule/${id}`, {
             headers: {
               'Accept': 'application/json'
@@ -80,16 +105,16 @@ export class API{
               password: this.accountPassword
             }
           });
-    
+
           return response.data;
          }catch(e){
         }
       }
-    
+
     public async getErrorLines(rootIndex: number, id:string, rulesource: string):Promise<sourceError[]>{
       const result:sourceError[] =[];
       try{
-       process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+       process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
         await axios.put(
           `${this.apis.serverList[rootIndex]['serverUrl']}/api/system/pipelines/rule/${id}`
           ,rulesource,
@@ -115,16 +140,16 @@ export class API{
               position_in_line: edata['position_in_line']
             };
             result.push(tempdata);
-          });          
+          });
         }
       }
       return result;
     }
 
-    
+
     public async getAllRules(url:string,token:string):Promise<[]>{
         try{
-         process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+         process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
           const response = await axios.get(`${url}/api/system/pipelines/rule`, {
             headers: {
               'Accept': 'application/json'
@@ -134,7 +159,7 @@ export class API{
               password: this.accountPassword
             }
           });
-  
+
           return response.data;
         }catch(e){
         }
@@ -144,7 +169,7 @@ export class API{
     async getFacilityAndServerVersion(rootIndex:number):Promise<{facility:string,version:string} | undefined>{
 
       try{
-       process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+       process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
         const response = await axios.get(`${this.apis.serverList[rootIndex].serverUrl}/api/system`, {
           headers: {
             'Accept': 'application/json'
@@ -161,12 +186,12 @@ export class API{
         };
       }catch(e){
       }
-      
+
       return undefined;
 
     }
     async getRuleConstraint(rootIndex:number,id: string){
-     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
         const response = await axios.post(`${this.apis.serverList[rootIndex].serverUrl}/api/system/content_packs/generate_id`, {},{
           headers: {
             'Accept': 'application/json',
@@ -183,7 +208,7 @@ export class API{
     }
 
     async createRule(rootIndex:number, title: string ):Promise<any>{
-     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
         const response = await axios.post(
             `${this.apis.serverList[rootIndex].serverUrl}/api/system/pipelines/rule`
             ,{
@@ -209,10 +234,10 @@ export class API{
         return null;
     }
 
-    
+
   public async getAllPipeLines(url:string,token:string):Promise<[]>{
       try{
-       process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+       process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
         const response = await axios.get(`${url}/api/system/pipelines/pipeline`, {
           headers: {
             'Accept': 'application/json'
